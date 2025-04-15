a0518003bf49e04f3236115da7994be4eb80394f
tungleqb <tungleqb@gmail.com>
2025-04-15 23:58:07 +0700

    fix b6 777

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -3,12 +3,15 @@ from fastapi.security import OAuth2PasswordBearer
 from ..config import settings
 from jose import JWTError, jwt
 from ..schemas.user import UserOut
+from ..database import get_db
+from ..services.auth import get_user_by_username
+from sqlalchemy.ext.asyncio import AsyncSession
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> UserOut:
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> UserOut:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -19,15 +22,14 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> UserOut:
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        return UserOut(id=1, username=username, ten_chi_nhanh="Test", is_admin=False)
+        db_user = await get_user_by_username(db, username)
+        if db_user is None:
+            raise credentials_exception
+        return UserOut.model_validate(db_user)
     except JWTError:
         raise credentials_exception
 
-def get_current_admin(token: str = Depends(oauth2_scheme)) -> UserOut:
-    user = get_current_user(token)
+async def get_current_admin(user: UserOut = Depends(get_current_user)):
     if not user.is_admin:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Không có quyền truy cập"
-        )
+        raise HTTPException(status_code=403, detail="Không có quyền truy cập")
     return user
