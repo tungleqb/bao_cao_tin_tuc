a28e01e1626034dc0e42c96538f077b030d634eb
tungleqb <tungleqb@gmail.com>
2025-04-15 07:05:22 +0700

    b4 fix3

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from .routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+# 👇 Thêm đoạn này
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 @app.get("/ping")
