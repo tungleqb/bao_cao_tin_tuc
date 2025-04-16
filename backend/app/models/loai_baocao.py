cb66270de108adde9614466007bfe354e6099766
tungleqb <tungleqb@gmail.com>
2025-04-16 23:25:37 +0700

    zone

--- backend/app/models/loai_baocao.py
+++ backend/app/models/loai_baocao.py
@@ -7,6 +7,7 @@ class LoaiBaoCao(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     ten_loai = Column(String, nullable=False)
-    thoi_gian_bat_dau = Column(DateTime, nullable=False)
-    han_gui = Column(DateTime, nullable=False)
+    from sqlalchemy import DateTime
+    thoi_gian_bat_dau = Column(DateTime(timezone=True), nullable=False)
+    han_gui = Column(DateTime(timezone=True), nullable=False)
     dinh_ky = Column(Integer, default=0)  # đơn vị giờ/ngày/tuần...
